f1d7f15251d8d8a71eebc6f0344dd190011a9fcc
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-11 19:04:56 -0500

    feat: preparar proyecto de react
    - instalar dependencias
    - configurar archivo vite.config.js
    -  agregar pagina componente App y componente Login

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+
+export function App () {
+    return (
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                </Routes>
+            </div>
+    )
+}
