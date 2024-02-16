ae412f6e6ca40ad7ee2231ad8206558e997d3834
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-15 20:02:49 -0500

    refactor(login): add link to register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
-export function Login() {
+import { Link } from 'react-router-dom'
+
+export function Login () {
     return (
         <>
             <h1>Login page</h1>
+            <Link to='/register'>go register</Link>
         </>
     )
 }
