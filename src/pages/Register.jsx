a6490caa1412e7959a1706517146f17d5cd32334
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-15 19:29:40 -0500

    fix(routes): add Register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+//import RegisterForm from '../features/authentication/components/RegisterForm'
+
+export function Register () {
+    return (
+        <>
+            <h1>Register</h1>
+            <p>Register Form</p>
+        </>
+    )
+}
