8bf7dc9318fa39aeb3301368a67cb16b5a9113bf
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-19 23:09:49 -0500

    refactor(main): Wrap RouterProvider with AuthProvider for authentication context
    Updated 'main.jsx' to wrap the 'RouterProvider' component with the 'AuthProvider' from './src/features/authentication/context/AuthContextProvider'. This change ensures that the authentication context is available throughout the application.

--- main.jsx
+++ main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './src/routes/routes'
 import React from 'react'
+import AuthProvider from './src/features/authentication/context/AuthContextProvider'
 
 createRoot(document.getElementById('app')).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
     </React.StrictMode>
 )
