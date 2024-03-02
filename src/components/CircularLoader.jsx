4ad01206ada50f85d5ec0ebdbb51ed4d1170b502
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-03-02 18:44:11 -0500

    feat: Add CircularLoader component and styles
    Added the CircularLoader component along with its corresponding CSS styles to the project. The CircularLoader component is designed to display a circular loading animation.
    
    Files added:
    - 'src/components/CircularLoader.jsx': Contains the CircularLoader component implementation.
    - 'src/components/CircularLoader.css': Contains the CSS styles for the CircularLoader component.

---
+++ src/components/CircularLoader.jsx
@@ -0,0 +1,11 @@
+import './CircularLoader.css'
+
+const CircularProgress = () => {
+    return (
+        <div className='circular-loader__wrapper'>
+            <span className="circular-loader"></span>
+        </div>
+    )
+}
+
+export default CircularProgress
